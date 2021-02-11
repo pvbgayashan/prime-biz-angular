0c95b602306866cedd34e963d53a2580f9569b26
Buddhika Gayashan <buddhika.gayashan@cloudsolution-sa.com>
2021-02-11 21:34:19 +0530

    ADD: items grid

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemsComponent } from './items.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     ItemsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AgGridModule,
+    ReactiveFormsModule
   ]
 })
 export class ItemsModule {}
