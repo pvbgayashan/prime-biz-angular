504d1b6d001b285154b45bde4e7e51820e12120d
Buddhika Gayashan <buddhika.gayashan@cloudsolution-sa.com>
2021-02-11 21:44:21 +0530

    ADD: pricing grid

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -7,8 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingComponent implements OnInit {
 
-  constructor() {}
+  public gridOptions: any;
+  public gridApi: any;
+  public rowData: any[] = [];
+  public columnDefs: any[] = [];
+  public defaultColDef: any;
 
-  ngOnInit(): void {}
+  constructor() {
+
+    // set grid options
+    this.gridOptions = {
+      filter: true,
+      context: {
+        parent: this
+      }
+    };
+
+    // set default col definition
+    this.defaultColDef = {
+      flex: 1,
+      minWidth: 150,
+      filter: true,
+      sortable: true,
+      floatingFilter: true
+    };
+
+  }
+
+  ngOnInit(): void {
+
+    // set column definition
+    this.setColumns();
+
+    // load data
+    this.loadData();
+
+  }
+
+  public gridReadyEvent(params: any) {
+    this.gridApi = params.api;
+
+    // adjust column sizes to fit
+    if (this.gridApi) {
+      this.gridApi.sizeColumnsToFit();
+    }
+  }
+
+  private setColumns() {
+    this.columnDefs = [
+      {
+        headerName: 'Name',
+        field: 'name',
+        filter: 'agTextColumnFilter',
+        suppressMenu: true
+      },
+      {
+        headerName: 'Active',
+        field: 'active',
+        filter: 'agTextColumnFilter',
+        suppressMenu: true
+      },
+      {
+        headerName: 'Actions',
+        field: 'actions',
+        floatingFilter: false,
+        suppressMenu: true
+      }
+    ];
+  }
+
+  private loadData() {}
+
+  public new() {}
 
 }
--- src/app/pricing/pricing.module.ts
+++ src/app/pricing/pricing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricingComponent } from './pricing.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     PricingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AgGridModule,
+    ReactiveFormsModule
   ]
 })
 export class PricingModule {}
