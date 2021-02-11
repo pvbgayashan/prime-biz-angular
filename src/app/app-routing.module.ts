0515058b8a96143c43610f07075728641a888abb
Buddhika Gayashan <buddhika.gayashan@cloudsolution-sa.com>
2021-02-11 17:31:38 +0530

    ADD: routing config to new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MasterDataComponent } from './master-data/master-data.component';
+import { BillingComponent } from './billing/billing.component';
+import { ServicesComponent } from './services/services.component';
+import { ItemsComponent } from './items/items.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
+  {
+    path: 'billing',
+    component: BillingComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'items',
+    component: ItemsComponent
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent
+  },
   {
     path: 'master-data',
     component: MasterDataComponent
