784dd30942d52931cd5c2ad605fb7290bfbb3df3
Buddhika Gayashan <buddhika.gayashan@cloudsolution-sa.com>
2021-03-07 15:19:04 +0530

    FIX: initial build time errors and warnings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,11 @@ import { FIREBASE_CONFIG } from '../../config/firebase.config';
 
 export const environment = {
   production: true,
-  firebase: FIREBASE_CONFIG.CONNECTION
+  firebase: FIREBASE_CONFIG.CONNECTION,
+  COLLECTION: {
+    MASTER_DATA: {
+      ITEM_TYPE: '/item-type',
+      ITEM_CATEGORY: '/item-category'
+    }
+  }
 };
